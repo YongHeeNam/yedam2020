1d7f9d257cda041ed8cc82b88788585089fa7339
User <User@YD02-04>
2020-01-21 12:37:20 +0900

    ssss

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		int age = 28;
+		int year = 2020;
+		System.out.println("올해는 " + year +"년,");
+		System.out.println("내 나이는 " + age + "입니다.");
+//		System.out.println("개노답");
+	}
+}
