6c6afdec86c7902ca6505ff8c71d14ed4b0ea781
com <com@DESKTOP-RGULE4H>
2020-01-29 17:49:15 +0900

    commit

---
+++ HelloWorld/src/reference/ArrayMethodExample.java
@@ -0,0 +1,29 @@
+package reference;
+
+public class ArrayMethodExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[]intAry = {3, 7, 5, 8, 4} ;
+	//	System.out.println("배열의 길이: " + intAry.length);
+		int sum = sumArrayMethod(intAry);
+		System.out.println("합은 : " + sum);
+		
+		int[]intAry2 = {13, 72, 5, 8, 4} ;
+		int sum2 = sumArrayMethod(intAry2);
+		System.out.println("합은 : " + sum2);
+		
+		int[]intAry3 = {33, 27, 45, 8, 44} ;
+		int sum3 = sumArrayMethod(intAry3);
+		System.out.println("합은 : " + sum3);
+		
+	} 
+		// 배열의 요소의 합을 계산하는 method.
+		public static int sumArrayMethod(int[] ary) {
+			int sum = 0;
+			for (int i= 0; i < ary.length; i++) {
+				sum += ary[i];
+			}
+			return sum;
+		}
+		}
---
+++ HelloWorld/src/reference/Excercise08.java
@@ -0,0 +1,26 @@
+package reference;
+
+public class Excercise08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] ary = { {95, 86}, 
+						{83, 92, 96},
+						{78, 83, 93, 87,88}
+		};
+		int sum = 0;
+		double avg = 0.0;
+		int count = 0;
+		for (int i = 0; i < ary.length; i++) {
+			//count += ary[i].lenght;
+			for (int j = 0; j < ary[i].length; j++) {
+				sum += ary[i][j];
+				count++;
+			}
+		}
+		avg = (double) sum /count;
+		System.out.println("sum: " + sum);
+		System.out.println("avg: " + avg);
+	}
+
+}
