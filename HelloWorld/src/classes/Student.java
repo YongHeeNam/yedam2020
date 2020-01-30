91d184c7ef7e855be8b9908218f19e32c5c9fb26
com <com@DESKTOP-RGULE4H>
2020-01-30 12:51:29 +0900

    mm

---
+++ HelloWorld/src/classes/Student.java
@@ -0,0 +1,74 @@
+package classes;
+
+public class Student {
+	// 필드
+	private String name; // private 직접접근제한 , 외부로부터 숨김
+	private int age;
+	private int height;
+	private int weight;
+
+	// 생성자(인스턴스를 만들면서 필드값을 초기화)
+	Student() {
+	}
+
+	Student(String name) {
+		this.name = name; // this는 Student의 필드를 의미한다
+	}
+
+	Student(String name, int age, int height, int weight) { // 4개의 필드값을 초기화
+		this.name = name;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+	}
+
+	// 메소드
+	void study() {
+		System.out.println("공부한당.");
+	}
+
+	void eat() {
+		System.out.println("밥먹는당.");
+	}
+
+	void sleep() {
+		System.out.println("잠을잔당.");
+	}
+
+	void setAge(int age) {
+		if (age >= 0)
+			this.age = age;
+		else
+			this.age = 0;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	int getAge() {
+		return age;
+	}
+	
+	
+}
---
+++ HelloWorld/src/classes/StudentArrayExample.java
@@ -0,0 +1,37 @@
+package classes;
+
+public class StudentArrayExample {
+	public static void main(String[] args) {
+	
+		Student[] stdAry = new Student[3]; //참조유형의 데이터Type
+		
+		
+		Student sn = new Student ("choi",20,180,70);
+		
+		Student stnt = new Student(); //student class의 인스턴스
+		stnt.setName("hong");
+		stnt.setAge(20);
+		stnt.setHeight(170);
+		stnt.setWeight(65);
+		
+		
+		Student snt = new Student();
+		snt.setName("park");
+		snt.setAge(43);
+		snt.setHeight(178);
+		snt.setWeight(70);
+		
+		
+		stdAry[0] = sn;
+		stdAry[1] = stnt;
+		stdAry[2] = snt;
+		
+		for(int i=0; i<stdAry.length; i++) {
+//			if(stdAry[i].getName.equals("choi"))
+			System.out.println(stdAry[i].getName() + "  " + stdAry[i].getAge());
+		}
+		
+		
+		
+	}
+	}
